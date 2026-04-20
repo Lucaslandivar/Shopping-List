1fdaafec54e93480157b332c9932d80ecff3ed4b
Lucas Landivar <landivarlucas67@gmail.com>
2026-04-20 15:42:52 -0300

    Configuring Route Parameters

--- course-project-cmp-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project-cmp-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,7 +8,7 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe; // problem fixed
+  recipe: Recipe; // problem fixed
 
   constructor(private recipeService: RecipeService) {}
 
